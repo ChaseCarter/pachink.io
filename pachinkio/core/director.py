30d6feb6b9aaa40f704b1ac06f9f9f426fe601d7
Chase Carter <chase.m.carter@gmail.com>
2023-02-18 14:46:22 -0600

    Read configs from env file; automatically update temp in interpolate_concepts

--- pachinkio/core/director.py
+++ pachinkio/core/director.py
@@ -6,13 +6,24 @@ from .embedding_client import EmbeddingClient
 
 class Director:
 
-    DEFAULT_TEMP = 1.55
+    DEFAULT_TEMP = 1.2
     completion_client: CompletionClient
     embedding_client: EmbeddingClient
     
     def __init__(self, completion_client: CompletionClient, embedding_client: EmbeddingClient):
         self.completion_client = completion_client
         self.embedding_client = embedding_client
+
+    def chain_of_intuition(self, query: str, fanout: int = 1):
+        base_prompt = "Steve Jobs: Yes, I always trust my intuition. Sometimes one intuition will naturally lead to another, and I can use this to solve complex problems. For example: \
+        1. I felt that people didn't want bulky phones, in spite of surveys showing that people wanted more functionality even at the expense of phone weight and size. \
+        2. I intuited that aesthetics and form factor might be more important than adding more functional features \
+        3. I could sense that the direction things were going in was towards elegant touchscreens and away from tactile buttons \
+        Another example is the way I followed my intuitions to discover {0}:"
+        prompt = base_prompt.format(query)
+        print("Prompt:", prompt)
+        results = self.completion_client.get_completions(prompt, n=fanout, temp=Director.DEFAULT_TEMP)
+        print(list(results))
 
     def compare_statements(self, statement1: str, statement2: str) -> float:
         base_embeddings = list(self.embedding_client.get_embeddings([statement1, statement2]))
@@ -27,12 +38,14 @@ class Director:
         target_embedding = base_embeddings[1][0]
         print("target embedding index:", base_embeddings[1][1])
         next_statement = start_statement
-        base_prompt = "Re-word this statement in a different way, for a different audience: '{0}'"
+        base_prompt = "Re-word this statement in a different way, but with similar sentence structure: '{0}'"
         best_similarity = cosine_similarity(start_embedding, target_embedding)
         iterations_without_progress = 0
+        temp = Director.DEFAULT_TEMP
 
         for i in range(iterations):
-            completions = list(self.completion_client.get_completions(base_prompt.format(next_statement), n=fanout, temp=Director.DEFAULT_TEMP))
+            print("Temp:", temp)
+            completions = list(self.completion_client.get_completions(base_prompt.format(next_statement), n=fanout, temp=temp))
             embeddings = self.embedding_client.get_embeddings(completions)
   
             similarity_to_target = map(lambda embedding: (cosine_similarity(embedding[0], target_embedding), embedding[1]), embeddings)
@@ -42,9 +55,11 @@ class Director:
                 best_similarity = best_completion_info[0]
                 next_statement = completions[best_completion_index]
                 iterations_without_progress = 0
+                temp = max(Director.DEFAULT_TEMP, temp * 0.9)
             else:
                 iterations_without_progress += 1
-                if iterations_without_progress > 4:
+                temp = min(temp * 1.1, 2.0)
+                if iterations_without_progress > 5:
                     print("*** Ending run early, too many iterations without progress toward target ***")
                     print(f"*** iterations: {i}")
                     break
--- pachinkio/core/oa_client.py
+++ pachinkio/core/oa_client.py
@@ -21,7 +21,7 @@ class OpenAIClient(CompletionClient, EmbeddingClient):
             engine=self.engine,
             prompt=prompt,
             temperature=temp,
-            max_tokens=40,
+            max_tokens=500,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0,

0efd45754fc06b0841a3437faa7722c86ca08a58
Chase Carter <chase.m.carter@gmail.com>
2023-03-03 14:09:46 -0600

    Extract app creation into factory method

--- app.py
+++ app.py
@@ -1,25 +1,30 @@
 from flask import Flask, jsonify, request
 from flask.wrappers import Response
 from flask_cors import CORS, cross_origin
+from injector import Injector
 
-if __name__ == '__main__':
-    from injector import Injector
+import pachinkio.api.api as api
+from pachinkio.core.director import Director
 
-    import pachinkio.api.api as api
-    from pachinkio.core.director import Director
-    from pachinkio.openai.oa_module import OpenAiApiModule
-    from pachinkio.prompts.prompts_module import PromptsModule
-
+def create_app(injector: Injector):
     app = Flask(__name__)
     cors = CORS(app)
     app.config['CORS_HEADERS'] = 'Content-Type'
 
     print("Initializing Flask application")
-    
-    injector = Injector([OpenAiApiModule, PromptsModule('./pachinkio/prompts/prompts_v1.ini')])
+
     director = injector.get(Director)
     api_blueprint = api.construct_blueprint(director)
 
     app.register_blueprint(api_blueprint, url_prefix='')
+
+    return app
+
+if __name__ == '__main__':
+    from pachinkio.openai.oa_module import OpenAiApiModule
+    from pachinkio.prompts.prompts_module import PromptsModule
+
+    injector = Injector([OpenAiApiModule, PromptsModule('./pachinkio/prompts/prompts_v1.ini')])
+    app = create_app(injector)
 
     app.run(debug = True)
